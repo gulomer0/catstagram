befd2bac89c5d7c1966c652250a5f0e61713997f
Ömer Gül <gul.omer1997@gmail.com>
2023-05-09 04:43:51 +0300

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       title: "Catstagram",
       debugShowCheckedModeBanner: false,
       theme: ThemeData
-          .light() /*.copyWith(
+          .dark() /*.copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),*/
       ,
