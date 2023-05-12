86973d904e6dd58c8d3eab24c5958ad939cd8c97
Ömer Gül <o.gul2020@gtu.edu.tr>
2023-05-12 03:31:24 +0300

    some changes

--- lib/screens/add_screen_2.dart
+++ lib/screens/add_screen_2.dart
@@ -18,6 +18,7 @@ class _AddScreen2State extends State<AddScreen2> {
     return Scaffold(
       appBar: AppBar(
         leading: Container(),
+        centerTitle: true,
         elevation: 0,
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         title: Image.asset(
@@ -25,67 +26,69 @@ class _AddScreen2State extends State<AddScreen2> {
           height: 40,
         ),
       ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: MediaQuery.of(context).size.width * 0.95,
+                    width: MediaQuery.of(context).size.width * 0.95,
+                    child: Image.asset(
+                      "assets/images/cat.png",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.98,
+              child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: MediaQuery.of(context).size.width * 0.95,
-                  width: MediaQuery.of(context).size.width * 0.95,
-                  child: Image.asset(
-                    "assets/images/cat.png",
-                    fit: BoxFit.cover,
+                child: TextField(
+                  cursorColor: Colors.orangeAccent,
+                  maxLines: 10,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.orangeAccent,
+                      ),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    filled: true,
+                    fillColor: Theme.of(context).scaffoldBackgroundColor,
+                    hintText: "Add a description...",
+                    hintStyle: TextStyle(color: Colors.grey),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.orangeAccent,
+                      ),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
                   ),
                 ),
               ),
-            ],
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.98,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                cursorColor: Colors.orangeAccent,
-                maxLines: 10,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.orangeAccent,
-                    ),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  filled: true,
-                  fillColor: Theme.of(context).scaffoldBackgroundColor,
-                  hintText: "Add a description...",
-                  hintStyle: TextStyle(color: Colors.grey),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.orangeAccent,
-                    ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.942,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text("Post"),
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  primary: Colors.orangeAccent,
+                  shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(5),
                   ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.942,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text("Post"),
-              style: ElevatedButton.styleFrom(
-                elevation: 0,
-                primary: Colors.orangeAccent,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5),
-                ),
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
